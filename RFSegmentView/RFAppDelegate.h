8a18cdcd1a2d6ead3883b484c022d0e9a4f4f739
王若风 <ouwakafeng@ouwakafengs-MacBook-Pro.local>
2015-01-15 10:31:53 +0800

    Initial Commit

---
+++ RFSegmentView/RFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RFAppDelegate.h
+//  RFSegmentView
+//
+//  Created by 王若风 on 1/15/15.
+//  Copyright (c) 2015 王若风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RFSegmentView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RFSegmentView
+//
+//  Created by 王若风 on 1/15/15.
+//  Copyright (c) 2015 王若风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RFAppDelegate class]));
+    }
+}
