c736e64b5805a0f27fd36938c208af0a3ed5549d
wangruofeng <261039521@qq.com>
2016-01-03 15:25:31 +0800

    update license info
    update license info

--- RFSegmentView/RFSegmentView/RFSegmentView.h
+++ RFSegmentView/RFSegmentView/RFSegmentView.h
@@ -1,9 +1,12 @@
 //
 //  RFSegmentView.h
-//  RFSegmentView
+//  RFSegmentView <https://github.com/wangruofeng/RFSegmentView>
 //
 //  Created by 王若风 on 1/15/15.
 //  Copyright (c) 2015 王若风. All rights reserved.
+//
+//  This source code is licensed under the MIT-style license found in the
+//  LICENSE file in the root directory of this source tree.
 //
 
 #import <UIKit/UIKit.h>
